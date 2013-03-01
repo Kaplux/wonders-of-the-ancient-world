7ac801a78b63fd593bfffb74e948f440f527efda
Kaplux <kaplux@gmail.com>
2013-03-01 12:10:17 +0100

    initial commit

---
+++ src/main/java/fr/mildlyusefulsoftware/wondersoftheworld/activity/ViewPictureActivity.java
@@ -0,0 +1,20 @@
+package fr.mildlyusefulsoftware.wondersoftheworld.activity;
+
+import android.os.Bundle;
+
+public class ViewPictureActivity extends fr.mildlyusefulsoftware.imageviewer.activity.ViewPictureActivity {
+
+	@Override
+	protected String getAdMobId() {
+		return "a150c8784ccf56a";
+	}
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//putNewPictures();
+	}
+
+
+}
